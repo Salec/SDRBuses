34dbb7b93caf0867637cf5155822091452a47571
Fernando Martin Garcia <f.martin@sofcloudit.com>
2017-10-04 17:52:42 +0200

    Basic Structure

---
+++ src/index.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Fernando on 4/10/2017.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+<Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+    </Provider>
+    , document.querySelector('.container'));
+
